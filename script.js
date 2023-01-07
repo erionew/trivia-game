1cc0e5457a158c1d170a2184d57dbdbf6ca75c9e
Erione White <erionewhite1999@gmail.com>
2023-01-06 21:17:10 -0600

    finished styling

---
+++ script.js
@@ -0,0 +1,34 @@
+const questionNumber = document.querySelector('.js-question-number')
+const score = document.querySelector('.js-score')
+
+const triviaQA = [
+    {
+        question: 'Which one of these cartoon dads have less than 3 children?',
+        answerChoices: ['Hank Hill', 'Bob Belcher', 'Peter Griffin', 'Homer Simpson'],
+        correctAnswer: ['Hank Hill']
+    },
+    {
+        question: 'What kind of fish was Nemo in the movie Finding Nemo?',
+        answerChoices: ['Surgeon Fish', 'Goldfish', 'Clown Fish', 'Beluga Whale'],
+        correctAnswer: ['Clown Fish']
+    },
+    {
+        question: '_____ was the second president of the United States.',
+        answerChoices: ['Abraham Lincoln', 'John Adams', 'Thomas Jefferson', 'John Quincy Adams'],
+        correctAnswer: ['John Adams']
+    },
+    {
+        question: 'Which of these celebrities has not served time in prison?',
+        answerChoices: ['Mariska Hargitay', 'R. Kelly', 'Martha Stewart', 'Lori Loughlin'],
+        correctAnswer: ['Mariska Hargitay']
+    },
+    {
+        question: 'All of these are Spider-Man villians EXCEPT for _____',
+        answerChoices: ['Green Goblin', 'Doctor Octopus', 'Man-Spider', 'The Thing'],
+        correctAnswer: ['The Thing']
+    },
+]
+
+document.addEventListener('load', () => {
+    
+})
